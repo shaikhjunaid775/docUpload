a4440ef559c58ad92c3c858c1b9a32a191aeaba0
shaikh juned <shaikhjunaid775@gmail.com>
2026-05-30 14:41:30 +0530

    configuring renderer

--- frontend/src/context/useAuth.jsx
+++ frontend/src/context/useAuth.jsx
@@ -67,7 +67,7 @@ export const AuthProvider = ({ children }) => {
     try {
       const response = await axios.post(
         // "http://localhost:3000/api/auth/login",
-        "https://docupload-scad.onrender.com/api/auth/login"
+        "https://docupload-scad.onrender.com/api/auth/login",
         {
           email,
           password,
